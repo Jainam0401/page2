aa4ed6f668d6eb85b0d17bb8a0b5f1719945f16d
Rashi Awasthi <rashiawasthi260@gmail.com>
2024-03-27 18:26:19 +0530

    Formbar by Rashi

--- app/CreateOrders/page.tsx
+++ app/CreateOrders/page.tsx
@@ -2,10 +2,12 @@ import Form from "../components/form";
 import Subnav from "../components/subnav";
 import DisplayNav from "../components/DisplayAddress";
 import BorderBottomComponent from "../components/BorderBottomComponent";
+import Formbar from "../components/Formbar";
 export default function CreateOrder() {
   return (
     <>
       <BorderBottomComponent />
+      <Formbar/>
       <div className="flex w-full h-full justify-between gap-3">
         <div className="w-full max-w-[80%]  bg-black">
           <Form />
