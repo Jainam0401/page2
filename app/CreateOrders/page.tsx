1131c8bf33050151f2913c5db33d9ee0473a975f
Shweta <shweta.pachpute@somaiya.edu>
2024-03-29 11:43:44 +0530

    css in form

--- app/CreateOrders/page.tsx
+++ app/CreateOrders/page.tsx
@@ -6,15 +6,17 @@ import Formbar from "../components/Formbar";
 export default function CreateOrder() {
   return (
     <>
-      <BorderBottomComponent />
+      <div className="flex-col md:flex-row lg:flex-row flex">
+        <BorderBottomComponent />
+      </div>
 
-      <div className="flex w-full h-full justify-between gap-3">
-        <div className="w-full max-w-[80%]">
-          <Form />
-        </div>
-        <div className="w-full max-w-[20%] h-[calc(100vh-48px)]  bg-white">
+      <div className="flex w-full h-full justify-between gap-3 md:w-[1/4] px-4 md:min-w-full lg:w-full sm:w-[3/5]">
+        {/* <div className="w-full max-w-[80%]"> */}
+        <Form />
+        {/* </div> */}
+        {/* <div className="w-full max-w-[20%] h-[calc(100vh-48px)]  bg-white">
           <DisplayNav />
-        </div>
+        </div> */}
       </div>
     </>
   );
--- app/components/BorderBottomComponent.tsx
+++ app/components/BorderBottomComponent.tsx
@@ -1,16 +1,12 @@
-import React from 'react';
+import React from "react";
 
 const BorderBottomComponent = () => {
   return (
-   
-      <div className="border-b border-dotted flex font-bold text-slate-500 mb-4  border-gray-300 m-auto">
-    
-          <span className="ml-10">Shipping details</span>
-          <span className="ml-10">Order details</span>
-          <span className="ml-10">Select Courier</span>
-        </div>
-   
-    
+    <div className="border-b border-dotted flex font-bold text-slate-500 mb-4  border-gray-300 m-auto ">
+      <span className="ml-10">Shipping details</span>
+      <span className="ml-10">Order details</span>
+      <span className="ml-10">Select Courier</span>
+    </div>
   );
 };
 
