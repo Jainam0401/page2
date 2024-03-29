da77fafae1ba98a37fbb735dffc10c35b19ed0f4
Jainam0401 <jainammithalaljain@gmail.com>
2024-03-29 15:53:06 +0530

    form by Jainam

--- app/components/Form.tsx
+++ app/components/Form.tsx
@@ -1,12 +1,44 @@
-import React from "react";
+'use client'
+import React, { useState } from "react";
 import Formbar from "./Formbar";
-import Button from "@/elements/Button";
-
 import { Label, Textarea, Checkbox } from "flowbite-react";
 
 function Form() {
+  const [formData, setFormData] = useState({
+    fullName: "",
+    email: "",
+    mobileNumber: "",
+    pincode: "",
+    state: "",
+    address: "",
+    date: "",
+    country: "",
+    cityName: "",
+    landmark: "",
+    others: "",
+    gstin: "",
+    shippingAddress: false,
+    billingAddress: false,
+  });
+
+  const handleInputChange = (e:any) => {
+    const { id, value, type, checked } = e.target;
+    const newValue = type === "checkbox" ? checked : value;
+    setFormData({ ...formData, [id]: newValue });
+  };
+
+  const handleSubmit = (e:any) => {
+    e.preventDefault();
+    console.log(formData); // You can replace this with your desired action (e.g., sending data to a server)
+    // Convert formData to JSON
+    const jsonData = JSON.stringify(formData, null, 2);
+    console.log(jsonData);
+  };
+
   return (
-    <div className="bg-white p-2 rounded-lg">
+    
+      <form onSubmit={handleSubmit}>
+      <div className="bg-white p-2 rounded-lg">
       <Formbar />
       <div className="border-2 py-2 border-black rounded-md px-4">
         <div className="">
@@ -18,7 +50,11 @@ function Form() {
               </label>
               <input
                 type="text"
-                id="full-name"
+                value={formData.fullName}
+                  onChange={handleInputChange}
+              
+                id="fullName"
+                 
                 className="border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
                 placeholder="Enter your full name"
               />
@@ -28,6 +64,8 @@ function Form() {
                 Email (Optional)
               </label>
               <input
+               value={formData.email}
+               onChange={handleInputChange}
                 type="email"
                 id="email"
                 className="border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
@@ -39,6 +77,8 @@ function Form() {
                 Mobile number
               </label>
               <input
+               value={formData.mobileNumber}
+               onChange={handleInputChange}
                 type="number"
                 id="number"
                 className="border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
@@ -47,7 +87,7 @@ function Form() {
             </div>
           </div>
         </div>
-        <div className="">
+        
           <p className="py-2 font-bold text-gray-700">Buyer Address Details</p>
           <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
             <div>
@@ -58,6 +98,8 @@ function Form() {
                 <input
                   type="number"
                   id="pincode"
+                  value={formData.pincode}
+                  onChange={handleInputChange}
                   className="border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
                   placeholder="Enter your Pin Code"
                 />
@@ -69,6 +111,8 @@ function Form() {
                 <input
                   type="text"
                   id="state"
+                  value={formData.state}
+                  onChange={handleInputChange}
                   className="border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
                   placeholder="Enter your State"
                 />
@@ -78,6 +122,8 @@ function Form() {
                 <Textarea
                   id="address"
                   placeholder="Leave a comment..."
+                  value={formData.address}
+                  onChange={handleInputChange}
                   required
                   rows={4}
                 />
@@ -89,6 +135,8 @@ function Form() {
                 <input
                   type="text"
                   id="date"
+                  value={formData.date}
+                  onChange={handleInputChange}
                   className="border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
                   placeholder="Enter your Date of"
                 />
@@ -100,6 +148,8 @@ function Form() {
                 <input
                   type="text"
                   id="country"
+                  value={formData.country}
+                  onChange={handleInputChange}
                   className="border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
                   placeholder="Enter your Country"
                 />
@@ -113,6 +163,8 @@ function Form() {
                 <input
                   type="text"
                   id="city"
+                  value={formData.cityName}
+                  onChange={handleInputChange}
                   className="border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
                   placeholder="Enter your City Name"
                 />
@@ -124,6 +176,8 @@ function Form() {
                 <input
                   type="text"
                   id="state"
+                  value={formData.state}
+                  onChange={handleInputChange}
                   className="border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
                   placeholder="Enter your State"
                 />
@@ -135,6 +189,8 @@ function Form() {
                   placeholder="Enter the landmark..."
                   required
                   rows={4}
+                  value={formData.landmark}
+                  onChange={handleInputChange}
                 />
               </div>
               <div>
@@ -146,6 +202,8 @@ function Form() {
                   id="others"
                   className="border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
                   placeholder=" Others"
+                  value={formData.others}
+                  onChange={handleInputChange}
                 />
               </div>
               <div>
@@ -157,11 +215,13 @@ function Form() {
                   id="gstin"
                   className="border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
                   placeholder=" Buyer's GSTIN"
+                  value={formData.gstin}
+                  onChange={handleInputChange}
                 />
               </div>
             </div>
           </div>
-        </div>
+        
       </div>
       <div className="flex ml-3 mt-2 items-center gap-2">
         <Checkbox id="shipping-address" />
@@ -173,9 +233,11 @@ function Form() {
       </div>
       <div className=" flex flex-col items-center space-y-5 md:flex-row md:space-x-5 md:space-y-0 md:p-4">
         <button className="w-[150px] border-[1px] border-red-400 py-2 text-red-400 md:ml-auto">Reset Changes</button>
-        <button className="w-[150px] bg-red-400 py-2">Continue</button>
+        <button type="submit" className="w-[150px]  bg-red-400 py-2">Continue</button>
       </div>
     </div>
+      </form>
+
   );
 }
 
